88b7590a60b799b6a5320466a099af758aa92184
camagma <ivan.zabila11@gmail.com>
2025-09-13 00:22:09 +0300

    zavdannya8.py

--- lesson2/zavdannya8.py
+++ lesson2/zavdannya8.py
@@ -1,5 +1,14 @@
 import random
-lst = []
-for i in range(10):
-    lst.append(random.randint(0, 100))
-print(lst)
+length = int(input("введіть довжину списку: "))
+firstlst = []
+newlst = []
+
+for i in range(length):
+    firstlst.append(random.randint(0, 100))
+
+print("перший список:", firstlst)
+
+for index in range(length):
+    newindex = length - 1 - index
+    newlst.append(firstlst[newindex])
+print("новий список:", newlst)
